85ed8e64f40a99dfed28aa84144ba57d2025b757
Vitalii Koreniev <v.korenev@otpusk.com>
2021-03-04 13:10:47 +0200

    added entities

---
+++ parser/src/Entity/Article.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArticleRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ArticleRepository::class)
+ */
+class Article {
+
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $content;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $created;
+
+    /**
+     * @ORM\Column(type="string", length=400)
+     */
+    private $url;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Category::class, inversedBy="articles")
+     */
+    private $categories;
+
+    public function __construct() {
+        $this->categories = new ArrayCollection();
+    }
+
+    public function getId(): ?int {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getContent(): ?string {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getCreated(): ?\DateTimeInterface {
+        return $this->created;
+    }
+
+    public function setCreated(\DateTimeInterface $created): self {
+        $this->created = $created;
+
+        return $this;
+    }
+
+    public function getUrl(): ?string {
+        return $this->url;
+    }
+
+    public function setUrl(string $url): self {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Category[]
+     */
+    public function getCategories(): Collection {
+        return $this->categories;
+    }
+
+    public function addCategory(Category $category): self {
+        if (!$this->categories->contains($category)) {
+            $this->categories[] = $category;
+        }
+
+        return $this;
+    }
+
+    public function removeCategory(Category $category): self {
+        $this->categories->removeElement($category);
+
+        return $this;
+    }
+
+}
